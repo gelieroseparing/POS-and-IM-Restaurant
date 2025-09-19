4e542298aceb03d9a8955d70714f8f3c6e9e2341
itsme___rose <gelieroseparing538@gmail.com>
2025-09-19 17:07:52 +0800

    Add frontend and clean up uploads

--- routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const CATEGORIES = ['Breakfast','Lunch','Dinner','Dessert','Drinks','Snack'];
-
-router.get('/', (_req, res) => {
-  res.json(CATEGORIES);
-});
-
-module.exports = router;
